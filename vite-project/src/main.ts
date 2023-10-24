2232aa51a42deb91b9862bcd175665448b4e1638
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-10-23 20:17:13 -0400

    Hack to play around w/ constructed styles!

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,24 +1,42 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let linkedCss = document.getElementById("shared-css");
+let globalRules = Array.from(linkedCss.sheet.cssRules).map(rule => rule.cssText).join("\n");
+let sharedCss = new CSSStyleSheet();
+sharedCss.replace(globalRules);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const tpl = document.getElementById("tpl-test");
+
+// const styles = document.getElementById("css");
+// const globalCSS = new CSSStyleSheet();
+// globalCSS.replaceSync(styles.textContent);
+// document.adoptedStyleSheets = [...document.adoptedStyleSheets, globalCSS];
+
+
+
+const btnShadow = document.querySelector("button[data-action=shadow]");
+btnShadow.addEventListener("click", async () => {
+  const el = document.getElementById("shadow");
+  const frag = tpl.content.cloneNode(true);
+  if (el.shadowRoot) {
+    el.shadowRoot.appendChild(frag);
+  } else {
+    el.attachShadow({mode: "open"}).appendChild(frag);
+
+    let ss = new CSSStyleSheet();
+    ss.replace(".green { color: green }");
+
+    el.shadowRoot.adoptedStyleSheets = [sharedCss, ss];
+    el?.classList.remove("hidden")
+  }
+});
+
+const btnLight = document.querySelector("button[data-action=light]");
+btnLight.addEventListener("click", () => {
+  const el = document.getElementById("light");
+  const frag = tpl.content.cloneNode(true);
+  el?.appendChild(frag);
+  el?.classList.remove("hidden")
+});
+
+
